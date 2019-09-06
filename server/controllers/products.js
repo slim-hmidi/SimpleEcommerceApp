6ef620bf478da7c0a3b1e2414debb376f40390e4
Slim Hmidi <slim.hmidi1@gmail.com>
2019-09-06 13:29:16 +0200

    add query instructions into try block

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -1,4 +1,4 @@
-const Product = require('../models/Product');
+const Product = require("../models/Product");
 
 /**
  * Create a new product
@@ -6,31 +6,26 @@ const Product = require('../models/Product');
  * @param {object} res - Express object
  */
 module.exports.createProduct = async (req, res) => {
-  const {
-    name,
-    price,
-    quantity,
-  } = req.body;
-
-  const newProduct = new Product({
-    name,
-    price,
-    quantity,
-  });
-
-  const foundProduct = await Product.findOne({ name });
-
-  // verify if a product already exits
-  if (foundProduct) {
-    return res.error(400, 'A product already exists');
-  }
+  const { name, price, quantity } = req.body;
 
   try {
+    const newProduct = new Product({
+      name,
+      price,
+      quantity
+    });
+
+    const foundProduct = await Product.findOne({ name });
+
+    // verify if a product already exits
+    if (foundProduct) {
+      return res.error(400, "A product already exists");
+    }
     const createdProduct = await Product.create(newProduct);
     if (createdProduct) {
       return res.success(201, createdProduct);
     }
-    return res.error(400, 'Unable to create the product');
+    return res.error(400, "Unable to create the product");
   } catch (error) {
     return res.error(500, error);
   }
@@ -45,15 +40,13 @@ module.exports.getProducts = async (req, res) => {
   try {
     const fetchedProducts = await Product.find({});
     if (fetchedProducts || !fetchedProducts.length) {
-      return res
-        .success(200, fetchedProducts);
+      return res.success(200, fetchedProducts);
     }
-    return res.error(400, 'Unable to get products');
+    return res.error(400, "Unable to get products");
   } catch (error) {
     return res.error(500, error);
   }
 };
-
 
 /**
  * Get a product
@@ -67,7 +60,7 @@ module.exports.getProduct = async (req, res) => {
     if (fetchedProduct) {
       return res.success(200, fetchedProduct);
     }
-    return res.error(404, 'Product not found');
+    return res.error(404, "Product not found");
   } catch (error) {
     return res.error(500, error);
   }
@@ -79,24 +72,25 @@ module.exports.getProduct = async (req, res) => {
  */
 module.exports.updateProduct = async (req, res) => {
   const { id } = req.params;
-  // check if the product exists
-  const fetchedProduct = await Product.findById(id);
-
-  // return an error message when the product does not exists
-  if (!fetchedProduct) {
-    return res.error(404, 'Product to update is not found!');
-  }
   try {
-    const updatedProduct = await Product.findOneAndUpdate(id, req.body, { new: true });
+    // check if the product exists
+    const fetchedProduct = await Product.findById(id);
+
+    // return an error message when the product does not exists
+    if (!fetchedProduct) {
+      return res.error(404, "Product to update is not found!");
+    }
+    const updatedProduct = await Product.findOneAndUpdate(id, req.body, {
+      new: true
+    });
     if (updatedProduct) {
       return res.success(200, updatedProduct);
     }
-    return res.error(400, 'Unable to update the Product');
+    return res.error(400, "Unable to update the Product");
   } catch (error) {
     return res.error(500, error);
   }
 };
-
 
 /**
  * Delete a product
@@ -109,11 +103,13 @@ module.exports.deleteProduct = async (req, res) => {
   try {
     const deletedProduct = await Product.findByIdAndDelete(id);
     if (deletedProduct) {
-      return res
-        // eslint-disable-next-line no-underscore-dangle
-        .success(200, deletedProduct._id);
+      return (
+        res
+          // eslint-disable-next-line no-underscore-dangle
+          .success(200, deletedProduct._id)
+      );
     }
-    return res.error(404, 'Product to delete is not found!');
+    return res.error(404, "Product to delete is not found!");
   } catch (error) {
     return res.error(500, error);
   }
