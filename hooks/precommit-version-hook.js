a4a2d50275baf26641793e298d2e9bd2f929e466
Slim Hmidi <slim.hmidi1@gmail.com>
2020-03-03 00:31:14 +0100

    add hook version script

---
+++ hooks/precommit-version-hook.js
@@ -0,0 +1,44 @@
+
+
+const { exec } = require('child_process');
+const path = require('path');
+const moment = require('moment');
+const fs = require('fs');
+
+function getBranch() {
+    return new Promise(((resolve, reject) => {
+        exec(
+            "git branch | grep '*'",
+            (err, stdout, stderr) => {
+                if (err) reject(err);
+                const name = stdout.replace('* ', '').replace('\n', '');
+                resolve(name);
+            },
+        );
+    }));
+}
+
+getBranch()
+    .then((branch) => {
+        if (branch === 'feat/package-version') {
+            const pathToFile = path.join(__dirname, '../package.json');
+            if (fs.existsSync(pathToFile)) {
+                const data = fs.readFileSync(pathToFile, 'utf8');
+                console.log(data)
+                const content = JSON.parse(data);
+                content.version = moment().format('YY.MM.DD');
+                console.log(content)
+                fs.writeFileSync(pathToFile, JSON.stringify(content));
+                exec(`git add ${pathToFile}`, (err, stdout, stderr) => {
+                    if (err) console.log(err);
+                    console.log(stdout);
+
+                })
+            } else {
+                console.log('Cannot find file : ', pathToFile);
+                return;
+            }
+        }
+        return;
+
+    });
